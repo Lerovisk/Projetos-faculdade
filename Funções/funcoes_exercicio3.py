83feff99640920b44021275fb583545a892fef3a
Leticia de O M Pereira <leeoliveiramessi@gmail.com>
2026-05-05 20:27:25 -0300

    Delete Funções directory

--- Funções/funcoes_exercicio3.py
+++
@@ -1,9 +0,0 @@
-'''Exercício de fixação 3: Crie um programa que receba uma lista de números e retorne a média.'''
-
-def media(*numeros):
-    ma = sum(numeros) / len(numeros)
-    return ma
-
-numeros = [2, 5, 9, 4, 11]
-result = media(*numeros)
-print(f"A media dos valores {numeros} é {result} ")
