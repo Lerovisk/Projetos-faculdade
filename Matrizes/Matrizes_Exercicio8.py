876416177ac4c099003eb5fc83e61ebf3241631e
Leticia de O M Pereira <leeoliveiramessi@gmail.com>
2026-05-05 20:26:29 -0300

    Delete Matrizes directory

--- Matrizes/Matrizes_Exercicio8.py
+++
@@ -1,9 +0,0 @@
-'''Exemplo de aplicação 8: Dada a matriz m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-efetue a soma de todos os seus elementos e exiba o resultado.'''
-
-m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-soma = 0
-for L in range(3):
-    for C in range(3):
-        soma += m[L][C]
-print("A soma dos elementos da matriz m é igual a", soma)
